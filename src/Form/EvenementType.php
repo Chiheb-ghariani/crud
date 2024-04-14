b1f64c8589b2882541631db89ee808767425c550
chiheb <gharianichiheb61@gmail.com>
2024-04-14 19:53:30 +0100

    fianle

--- src/Form/EvenementType.php
+++ src/Form/EvenementType.php
@@ -8,7 +8,11 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use App\Entity\Espacepartenaire;
-
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Form\Extension\Core\Type\TextType; // Add this line
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class EvenementType extends AbstractType
 {
@@ -22,9 +26,37 @@ class EvenementType extends AbstractType
                 // Add more options as needed
             ])
             ->add('nom_event')
-            ->add('date_event')
-            ->add('capacite')
-            ->add('description');
+            ->add('date_event' ,TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Date cannot be blank']),
+                    new Regex([
+                        'pattern' => '/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(202[4-9]|20[3-9][0-9]|[3-9][0-9]{3})$/',
+                        'message' => 'Please enter a valid date in the format dd/mm/yyyy.',
+                    ]),
+                ],
+            ])
+             
+            
+            ->add('capacite', IntegerType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Range([
+                        'min' => 5,
+                        'minMessage' => 'The capacity must be at least {{ limit }} persones.',
+                    ]),
+                ],
+            ])
+            ->add('description', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z]+$/',
+                        'message' => 'The description should contain only letters.',
+                    ]),
+                ],
+            ])
+            
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -9,16 +9,46 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType; // Import EntityType
 use App\Entity\Evenement;
 use App\Entity\User;
-
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Validator\Constraints\Range;
 class ReservationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('nombreplace')
-            ->add('nom_user')
-            ->add('prenom_user')
-            ->add('age')
+            ->add('nom_user', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z]+$/',
+                        'message' => 'The user name should contain only letters.',
+                    ]),
+                ],
+            ])
+
+
+            ->add('prenom_user', TextType::class, [ // Add this line
+                'constraints' => [ // Add this line
+                    new NotBlank(), // Add this line
+                    new Regex([ // Add this line
+                        'pattern' => '/^[a-zA-Z]+$/', // Add this line
+                        'message' => 'The user surname should contain only letters.', // Add this line
+                    ]), // Add this line
+                ], // Add this line
+            ]) // Add this line
+            ->add('age', IntegerType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Range([
+                        'min' => 16,
+                        'minMessage' => 'The user must be at least {{ limit }} years old.',
+                    ]),
+                ],
+            ])
             
           // Replace the id_event field with EntityType
           ->add('id_event', EntityType::class, [
